a7d1094d40a0f5602258b582ebf73a7b99d85b4e
Alice Freda <alicefreda@gmail.com>
2019-10-23 19:40:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,14 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   elsif array.length > 0
-    array.map!.with_index {|name, index| "#{index+1}. #{name}" }
+    array.map.with_index {|name, index| "#{index+1}. #{name}" }
     puts "The line is currently: #{array.join(" ")}"
   end
 end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  number = katz_deli.index("#{name}")+1
+  number = katz_deli.index(name)+1
   puts "Welcome, #{katz_deli.last}. You are number #{number} in line."
 end
 
